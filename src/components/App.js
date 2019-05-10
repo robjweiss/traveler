eca6ff995b3c360851efb80017f5e921aa63ab9d
robjweiss <rweiss55@gmail.com>
2019-05-10 13:10:13 -0400

    app component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a className="navbar-brand" href="#">
+          Traveler
+        </a>
+      </nav>
+    </div>
+  );
+}
