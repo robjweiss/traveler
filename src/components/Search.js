818a03ec40197b5e47c9309cee7be27d0da579e7
robjweiss <rweiss55@gmail.com>
2019-05-16 13:08:19 -0400

    added autosuggestion for places

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "./Form";
 import Results from "./Results";
+import rapidapi from "../../config/rapidapi";
 
 class Search extends React.Component {
     constructor(props) {
@@ -8,21 +9,38 @@ class Search extends React.Component {
 
         this.state = {
             from: '',
+            fromSuggestions: [],
             to: '',
+            toSuggestions: [],
             submitted: false
         }
 
         this.fromChange = this.fromChange.bind(this);
         this.toChange = this.toChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.getPlaceSuggestions = this.getPlaceSuggestions.bind(this);
     }
 
-    fromChange(event) {
+    async fromChange(event) {
         this.setState({from: event.target.value});
+
+        try {
+            const suggestions = await this.getPlaceSuggestions(event.target.value);
+            this.setState({fromSuggestions: suggestions});
+        } catch(e) {
+            console.log(e);
+        }
     }
 
-    toChange(event) {
+    async toChange(event) {
         this.setState({to: event.target.value});
+
+        try {
+            const suggestions = await this.getPlaceSuggestions(event.target.value);
+            this.setState({toSuggestions: suggestions});
+        } catch(e) {
+            console.log(e);
+        }
     }
 
     handleSubmit(event) {
@@ -31,13 +49,41 @@ class Search extends React.Component {
         this.setState({submitted: true});
     }
 
+    async getPlaceSuggestions(query) {
+        // 2 characters is the minimum query length allowed by the API
+        if(query.length >= 2) {
+            const url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/USA/USD/en-US/?query=" + query;
+            const headers = rapidapi;
+            try {
+                const res = await fetch(url, {headers: headers});
+                const data = await res.json();
+
+                const suggestions = data.Places.map((place) => {
+                    return(
+                        <option key={place.PlaceId} value={place.PlaceId} label={place.PlaceName}/>
+                    );
+                });
+
+                return suggestions;
+            } catch(e) {
+                console.log(e);
+            }
+        }
+        // Reset the suggestions if the character count falls below 2
+        else {
+            return [];
+        }
+    }
+
     render() {
         if (!this.state.submitted) {
             return (
                 <div>
                     <Form
                         from={this.state.from}
+                        fromSuggestions={this.state.fromSuggestions}
                         to={this.state.to}
+                        toSuggestions={this.state.toSuggestions}
                         fromChange={this.fromChange}
                         toChange={this.toChange}
                         handleSubmit={this.handleSubmit}/>
