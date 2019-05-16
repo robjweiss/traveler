e9185388b5173354f7b6613228d93050bf88c147
robjweiss <rweiss55@gmail.com>
2019-05-15 23:07:50 -0400

    search form component

---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <form class="form-inline justify-content-center" onSubmit={this.props.handleSubmit.bind(this)}>
+                <div>
+                    <label class="sr-only" for="from">From</label>
+                    <div class="input-group mb-2 mr-sm-2">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">From</div>
+                    </div>
+                    <input type="text" class="form-control" id="from" value={this.props.from} onChange={this.props.fromChange.bind(this)} />
+                    </div>
+                </div>
+
+                <div>
+                    <label class="sr-only" for="to">To</label>
+                    <div class="input-group mb-2 mr-sm-2">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">To</div>
+                    </div>
+                    <input type="text" class="form-control" id="to" value={this.props.to} onChange={this.props.toChange.bind(this)} />
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
+        );
+    }
+}
+
+export default Form;
