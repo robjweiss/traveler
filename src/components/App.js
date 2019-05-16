2f036b7662b4e1bbf42dd307506f82357c8df1e3
robjweiss <rweiss55@gmail.com>
2019-05-15 23:09:47 -0400

    refactored to contain Search component

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,25 @@ import Search from "./Search";
 export default function App() {
   return (
     <div>
-      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a className="navbar-brand" href="#">
-          Traveler
-        </a>
-      </nav>
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a className="navbar-brand" href="#">
+        Traveler
+      </a>
+    </nav>
 
-      <main role="main">
-        <div className="jumbotron">
-          <div className="container">
-            <h1 className="display-3">Go anywhere</h1>
-            <p>Enter an origin and a desination</p>
-          </div>
-        </div>
-
-        <form class="form-inline justify-content-center">
-          <Search label={"From"}/>
-          <Search label={"To"}/>
-          <button type="submit" class="btn btn-primary mb-2">Submit</button>
-        </form>
-
+    <main role="main">
+      <div className="jumbotron">
         <div className="container">
+          <h1 className="display-3">Go anywhere</h1>
+          <p>Enter an origin and a desination</p>
         </div>
-      </main>
-    </div>
+      </div>
+
+      <Search/>
+
+      <div className="container">
+      </div>
+    </main>
+  </div>
   );
 }
